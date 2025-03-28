fa90e60c44b44c215d39595be6063853970c9f06
Al Cerdan Lico <alcerdan@yahoo.com>
2025-03-28 13:52:58 -0700

    add flask

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
-# from flask_cors import CORS
+from flask_cors import CORS
 # from flask_migrate import Migrate
 # from sqlalchemy import MetaData
 
