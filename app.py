164a7159adafed6abf81f33e2ace198fc22f70f0
Al Cerdan Lico <alcerdan@yahoo.com>
2025-03-28 13:47:45 -0700

    add flask

--- app.py
+++ app.py
@@ -1,67 +1,35 @@
-# from flask import Flask
-# from flask_restful import Api
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_cors import CORS
-# from flask_migrate import Migrate
-# from sqlalchemy import MetaData
-
-# # Instantiate app and set configurations
-# app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# app.config['SECRET_KEY'] = 'secret_key'
-# app.json.compact = False
-
-# # Define metadata, instantiate db
-# metadata = MetaData(naming_convention={
-#     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-# })
-# db = SQLAlchemy(metadata=metadata)
-# migrate = Migrate(app, db)
-# db.init_app(app)
-
-# # Instantiate REST API and CORS
-# api = Api(app)
-# CORS(app)
-
-# @app.route('/')
-# def home():
-#     return "Flask heroku app."
-
-# if __name__ == '__main__':
-#     from os import environ
-#     app.run(host='0.0.0.0', port=int(environ.get('PORT', 5000)))
+import os
 
 from flask import Flask
-# from flask_restful import Api
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_cors import CORS
-# from flask_migrate import Migrate
-# from sqlalchemy import MetaData
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from flask_migrate import Migrate
+from sqlalchemy import MetaData
 
 # Instantiate app and set configurations
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///app.db')
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # app.config['SECRET_KEY'] = 'secret_key'
-# app.json.compact = False
+app.json.compact = False
 
-# # Define metadata, instantiate db
-# metadata = MetaData(naming_convention={
-#     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-# })
-# db = SQLAlchemy(metadata=metadata)
-# migrate = Migrate(app, db)
-# db.init_app(app)
+# Define metadata, instantiate db
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+db = SQLAlchemy(metadata=metadata)
+migrate = Migrate(app, db)
+db.init_app(app)
 
-# # Instantiate REST API and CORS
-# api = Api(app)
-# CORS(app)
+# Instantiate REST API and CORS
+api = Api(app)
+CORS(app)
 
 @app.route('/')
 def home():
     return "Flask heroku app."
 
 if __name__ == '__main__':
-    from os import environ
-    app.run(host='0.0.0.0', port=int(environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
